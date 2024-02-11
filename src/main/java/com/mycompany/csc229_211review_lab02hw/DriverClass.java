f4a924464c16e9163ef857124c1460fe5d0f007b
Jaspreet Singh <singhj0604@gmail.com>
2024-02-10 23:21:20 -0500

    Finish

--- src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/DriverClass.java
@@ -1,5 +1,7 @@
 package com.mycompany.csc229_211review_lab02hw;
 
+import java.util.Scanner;
+
 /**
  *
  * @author MoaathAlrajab
@@ -7,22 +9,15 @@ package com.mycompany.csc229_211review_lab02hw;
 public class DriverClass {
 	
 	public static void main(String[] args) {
-		// ToDo 5: Fix the error
-		
-		// ToDo 6: Fix the constructor of Student class
-		
-		// ToDo 7: Add a toString method for Student class
-		
-		Student std1= new Student("James", 20);
-		
-		// ToDo 8: Set the gpa of the student using the scanner and user
-		// 			input and then print the output.
-		
-		System.out.println(std1);
-		
-		// ToDo 9: add comments and explain your code
-		
-		// ToDo 10: submit using a pull request.
+		Scanner scanner = new Scanner(System.in);
+		Student std1= new Student("James", (short) 20); // Making a new Student object with the name and age
+
+		System.out.print("Enter GPA for " + std1.getName() + ": "); // Getting the user to give a GPA value for the student
+		double GPA = scanner.nextDouble();
+		std1.setGPA(GPA);
+
+		System.out.println(std1); // Printing the student information
+
 	}
 
 }
--- src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
+++ src/main/java/com/mycompany/csc229_211review_lab02hw/Student.java
@@ -4,13 +4,34 @@ package com.mycompany.csc229_211review_lab02hw;
  *
  * @author MoaathAlrajab
  */
-public class Student {
-	// ToDo 1: Make this class a child of Person
-	
-	// ToDo 2: Fix the resulting errors
-	
-	// ToDo 3: Add a field for GPA and create setter and getter
-	
-	// ToDo 4: Add comments to your code
+public class Student extends Person { // ToDo 1: Make this class a child of Person
+
+    private double GPA; // ToDo 3: Add a field for GPA
+
+    public Student(String name, short age) { // Constructor
+        super(name, age); // Calls the constructor of the superclass
+    }
+
+    public double getGPA(){ // ToDo 3: Getter for GPA
+        return GPA;
+    }
+
+    public void setGPA(double GPA){ // ToDo 3: Setter for GPA
+        this.GPA = GPA;
+    }
+
+    @Override // ToDo 7: Add a toString method for Student class
+    public String getAddress(){
+        return null;
+    }
+
+    @Override // ToDo 7: Add a toString method for Student class
+    public void setAddress(String address) {
+    }
+
+    @Override // ToDo 7: Add a toString method for Student class
+    public String toString(){
+        return "Student: " + getName() + ", Age: " + getAge() + ", GPA: " + GPA;
+    }
 
 }
